5499313828b610cff2ac528a863e3d7a22d6bf7b
Dev.Wolph <43886037+H3NRILiN@users.noreply.github.com>
2024-11-03 23:30:35 +0800

    Add argument reading
    1st argument is input game directory (currently forced to set this argument)
    2nd argument is output directory (optional, if not exists, it'll create new directory)

--- to_json2.rb
+++ to_json2.rb
@@ -8,16 +8,26 @@
 require 'jsonable'
 require 'zlib'
 require_relative 'rgss3'
+
+begin
+path = File.expand_path(ARGV[0])
+targetPath = ARGV[1]
+if targetPath
+  targetPath = File.expand_path(targetPath)
+end
+
+p 'Read '+path
+
 [
   'Data/Actors.rvdata2',
   'Data/Animations.rvdata2',
-#  'Data/Areas.rvdata2',
+  'Data/Areas.rvdata2',
   'Data/Armors.rvdata2',
   'Data/Classes.rvdata2',
   'Data/CommonEvents.rvdata2',
   'Data/Enemies.rvdata2',
   'Data/Items.rvdata2',
-  *Dir.glob('Data/Map[0-9][0-9][0-9].rvdata2'),
+  *Dir.glob(path+'/Data/Map[0-9][0-9][0-9].rvdata2'),
   'Data/MapInfos.rvdata2',
   'Data/Skills.rvdata2',
   'Data/States.rvdata2',
@@ -26,26 +36,56 @@ require_relative 'rgss3'
   'Data/Troops.rvdata2',
   'Data/Weapons.rvdata2'
 ].each do |rvdata|
+  if rvdata[path]
+  else
+    rvdata = File.join(path,rvdata)
+  end
   data = ''
-  p rvdata
-  File.open(rvdata, 'rb') do |file|
-    data = Marshal.load(file.read)
-    if data.is_a?(Array)
-	    data.each{ |d|
-	    	d.unpack_names if d != nil
-	    }
-		elsif data.is_a?(Hash)
-			if data.size != 0
-				data.each_value{|v|
-					v.unpack_names
-				}
-			end
-		else
-			data.unpack_names
+  p 'Convert: '+rvdata
+  begin
+    File.open(rvdata, 'rb') do |file|
+      data = Marshal.load(file.read)
+      if data.is_a?(Array)
+    	    data.each{ |d|
+    	    	d.unpack_names if d != nil
+    	    }
+    		elsif data.is_a?(Hash)
+    			if data.size != 0
+    				data.each_value{|v|
+    					v.unpack_names
+    				}
+    			end
+    		else
+    			data.unpack_names
+      end
     end
-  end
-  File.open('Data/'+File.basename(rvdata,'.rvdata2')+'.json', 'w') do |file|
-    file.write(data.to_json)
-  end
-end
 
+    curDir = path+'/Data/'
+
+    if targetPath
+      curDir = targetPath
+    end
+
+    if !curDir.end_with?("/")
+      curDir = curDir + '/'
+    end
+
+    p curDir
+
+    unless Dir.exist?(curDir)
+      Dir.mkdir(curDir)
+    end
+    File.open(curDir+File.basename(rvdata,'.rvdata2')+'.json', 'w') do |file|
+        file.write(data.to_json)
+    end
+  rescue => error
+      p "Faild, code:"
+      p "err|file=#{rvdata}|errorclass=#{error.class}|error=#{error.message}"
+    next
+  end
+  p 'complete!'
+end
+rescue => error
+  p error
+  p 'Failed to complete'
+end
